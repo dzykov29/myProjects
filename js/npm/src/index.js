6282f899c3d49d1280e3690ff626e0685e585a05
Dmitriy Zykov <zadima96@gmail.com>
2023-12-14 14:50:37 +0500

    add lint

--- js/npm/src/index.js
+++ js/npm/src/index.js
@@ -1,82 +1,80 @@
-import { el, setChildren} from 'redom'
+import { el, setChildren } from 'redom';
 import Navigo from 'navigo';
 
-const router = new Navigo('/')
+const router = new Navigo('/');
 
-const h1 = el('h1', 'Каталог товаров')
+const h1 = el('h1', 'Каталог товаров');
 
 setChildren(window.document.body, h1);
 
 function getProductList() {
-    const body = el('div', 'loading');
+  const body = el('div', 'loading');
 
-    fetch('https://fakestoreapi.com/products')
-        .then(async (res) => {
-            const data = await res.json();
+  fetch('https://fakestoreapi.com/products')
+    .then(async (res) => {
+      const data = await res.json();
 
-            const ul = el('ul');
+      const ul = el('ul');
 
-            setChildren(ul, data.map(product => el(
-                'li', 
-                el('a', {
-                    href: `/products/${product.id}`,
-                    onClick(event) {
-                        event.preventDefault();
-                        router.navigate(event.target.getAtribute('href'));
-                    } 
-                    },
-                    product.title)
-                )));
-            body.innerHTML = '';
-            setChildren(body, ul);
-        })
+      setChildren(ul, data.map((product) => el(
+        'li',
+        el('a', {
+          href: `/products/${product.id}`,
+          onClick(event) {
+            event.preventDefault();
+            router.navigate(event.target.getAtribute('href'));
+          }
+        },
+        product.title),
+      )));
+      body.innerHTML = '';
+      setChildren(body, ul);
+    });
 
-    return el('div', [
-        el('h1', 'Product list'),
-        body,
-    ])
+  return el('div', [
+    el('h1', 'Product list'),
+    body,
+  ]);
 }
 
 function catalogDetails(id) {
-    const body = el('div', 'loading');
+  const body = el('div', 'loading');
 
-    fetch(`https://fakestoreapi.com/products/${id}`)
-        .then(async (res) => {
-            const data = await res.json();
+  fetch(`https://fakestoreapi.com/products/${id}`)
+    .then(async (res) => {
+      const data = await res.json();
 
-            body.innerHTML = '';
+      body.innerHTML = '';
 
-            setChildren(body, [
-                el('a', {
-                    href: `/`,
-                    onClick(event) {
-                        event.preventDefault();
-                        router.navigate(event.target.getAtribute('href'));
-                    }
-                }, 'Back to list'),
-                el('h2', data.title),
-                el('p', data.description),
-                el('img', {
-                    src: data.image,
-                    alt: data.title,
-                })
-            ])
-        })
+      setChildren(body, [
+        el('a', {
+          href: `/`,
+          onClick(event) {
+            event.preventDefault();
+            router.navigate(event.target.getAtribute('href'));
+          },
+        }, 'Back to list'),
+        el('h2', data.title),
+        el('p', data.description),
+        el('img', {
+          src: data.image,
+          alt: data.title,
+        }),
+      ]);
+    });
 
-    return el('div', [
-        el('h1', `Product details ${id}`),
-        body,
-    ])
+  return el('div', [
+    el('h1', `Product details ${id}`),
+    body,
+  ]);
 }
 
 router.on('/', () => {
-    setChildren(window.document.body, getProductList());
+  setChildren(window.document.body, getProductList());
 });
 
 router.on('/products/:id', ({ data: { id } }) => {
-    setChildren(window.document.body, catalogDetails(id));
-})
+  setChildren(window.document.body, catalogDetails(id));
+});
 
 router.resolve();
-
-
